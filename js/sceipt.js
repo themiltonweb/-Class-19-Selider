428a23d7f0154a022ef495b8b0f8390ec7b9ee74
Milton_Web <114828490+themiltonweb@users.noreply.github.com>
2022-11-16 10:48:54 +0600

    Add files via upload

---
+++ js/sceipt.js
@@ -0,0 +1,16 @@
+$(function(){
+
+
+    $('.slider_part').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows:true,
+        prevArrow:'<i class="fa-solid fa-circle-chevron-right prev"></i>',
+        nextArrow:'<i class="fa-solid fa-circle-chevron-left next"></i>',
+        dots:true
+      });
+
+
+});
